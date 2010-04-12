c0cecbbac40a0d7eb22b85686e86d37946dc00dd
michael <ma@zive.at>
2010-04-12 02:06:40 +0200

    nifty DataTable#group_by transformation

--- lib/data_table.rb
+++ lib/data_table.rb
@@ -1,22 +1,46 @@
 require 'fastercsv'
 
 class DataTable
-  
   class ColumnAlreadyExists < StandardError ; end
   class ColumnNotFound < StandardError ; end
-
+  
+  INFINITY = 1.0/0
+  
+  class Aggregators
+    SUM = lambda do |key, rows| 
+      result = 0.0
+      rows.each { |r| result += r[key] }
+      result
+    end
+    
+    MIN = lambda do |key, rows| 
+      result = INFINITY
+      rows.each { |r| result = r[key] if r[key] < result }
+      result
+    end
+    
+    MAX = lambda do |key, rows| 
+      result = -INFINITY
+      rows.each { |r| result = r[key] if r[key] > result }
+      result
+    end
+  end
+  
   class Column
     attr_accessor :key
     attr_accessor :header
     attr_accessor :transformer
 
-    def initialize(header, transformer)
+    def initialize(key, header, transformer)
+      @key = key
       @header = header
       @transformer = transformer
     end
   end
   
   class Row
+    attr_accessor :cells
+    
     def initialize(table)
       @table = table
       @cells = {}
@@ -35,6 +59,8 @@ class DataTable
   end
   
   attr_accessor :columns
+  attr_accessor :rows
+  
   def initialize
     @columns = {}
     @column_keys = [] # used to memorize the order of columns
@@ -44,10 +70,10 @@ class DataTable
   def add_column(key, header, transformer = nil)
     raise ColumnAlreadyExists if @columns.has_key?(key)
     @column_keys << key
-    @columns[key] = Column.new(header, transformer)
+    @columns[key] = Column.new(key, header, transformer)
   end
   
-  def build_rows!(items)
+  def build_rows(items)
     items.each do |item|
       row = Row.new(self)
       @columns.each do |key, col|
@@ -62,6 +88,43 @@ class DataTable
     end
   end
   
+  # aggregates a set of rows using aggregator functions
+  def aggregate(key, rows, aggregators, group_key)
+    aggregated_row = Row.new(self)
+    
+    aggregated_row[key] = group_key.call(rows.first[key])
+    
+    aggregators.each do |key, aggregator|
+      aggregated_row[key] = aggregator.call(key, rows)
+    end
+
+    aggregated_row
+  end
+  
+  # yields a grouped DataTable object
+  def group_by(key, aggregators, group_key = lambda {|x| x })
+    result = DataTable.new
+    my_key = key
+    groups = {}
+    
+    @rows.each do |row|
+      idx = group_key.call(row[key])
+      groups[idx] = groups[idx].to_a << row
+    end
+    
+    # copy implicitly selected colums from original data table
+    result.add_column(key, @columns[key].header)
+    aggregators.each_key do |key|
+      result.add_column(key, @columns[key].header)
+    end
+    
+    groups.each_value do |group|
+      result.rows << aggregate(my_key, group, aggregators, group_key) # pick first row # aggregate
+    end
+    
+    result
+  end
+  
   def render_csv(options)
     FasterCSV.generate(options) do |csv|
       # Add headers
@@ -69,6 +132,7 @@ class DataTable
       @column_keys.each do |key|
         headers << @columns[key].header
       end
+      
       csv << headers
       
       # Add data rows
@@ -83,3 +147,5 @@ class DataTable
     end
   end
 end
+
+
--- test/test_data_table.rb
+++ test/test_data_table.rb
@@ -7,34 +7,58 @@ class TestDataTable < Test::Unit::TestCase
       class OrderPosition
         attr_accessor :articlenum
         attr_accessor :name
+        attr_accessor :ordered
+        attr_accessor :group
         attr_accessor :quantity
         attr_accessor :price
       
-        def initialize(articlenum, name, quantity, price)
+        def initialize(articlenum, name, group, ordered, quantity, price)
           @articlenum = articlenum
           @name = name
+          @group = group
+          @ordered = ordered
           @quantity = quantity
           @price = price
         end
       end
-    
-      @order_positions = [ OrderPosition.new("AB36KAA", "Laser Sword", 4, 1231.45), OrderPosition.new("D81ADG7A", "R2DX", 2, 42001.04 ) ]
+      
+      @order_positions = [ 
+        OrderPosition.new("AB36KAAA", "Laser Sword", "AA", Date.new(2009, 10, 4), 1, 1000.0),
+        OrderPosition.new("D81ADG7A", "R2DX",        "AA", Date.new(2009, 9, 4),  1, 100.0 ),
+        OrderPosition.new("D81ADG7A", "R2DX",        "AA", Date.new(2009, 9, 4),  1, 10.0 ),
+        OrderPosition.new("DTJA6181", "R2DX",        "BB", Date.new(2009, 10, 4), 1, 200.0 ),
+        OrderPosition.new("DTJA6181", "R2DX",        "BB", Date.new(2009, 10, 4), 1, 20.0 )
+      ]
     end
-  
+    
     should "work properly" do
+      
       t = DataTable.new
+      
       # variant 1: implicit method lookup
       t.add_column(:articlenum, "Articlenum")
       t.add_column(:name, "Product Name")
+      t.add_column(:ordered, "Datum")
+      t.add_column(:group, "Product Group")
       # variant 2: explicit transformer function that yields an item of your collection 
       t.add_column(:amount, "Amount", lambda { |p| p.quantity*p.price })
-      # variant 3: explicit row modification see build_rows! 
+      # variant 3: explicit row modification (see build_rows!)
       t.add_column(:doubled_price, "Doubled Price")
       
-      t.build_rows!(@order_positions) do |row, p| # block can be omitted
+      t.build_rows(@order_positions) do |row, p| # block can be omitted
         row[:doubled_price] = p.price*2
       end
-      puts t.render_csv(:col_sep => ";")
+      
+      grouped_table = t.group_by(:ordered, {
+        :amount => DataTable::Aggregators::SUM,
+        :doubled_price => DataTable::Aggregators::SUM
+      }, lambda {|x| x.year.to_s+"-"+x.month.to_s})
+      
+      # puts t.render_csv(:col_sep => ",")
+      puts grouped_table.render_csv(:col_sep => ",")
     end
+    
+    
+    
   end
 end
